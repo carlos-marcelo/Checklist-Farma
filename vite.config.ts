94de94db643dd317115301f3e378f60d56898e0f
carlos-marcelo <62825458+carlos-marcelo@users.noreply.github.com>
2025-12-03 18:23:07 -0300

    online

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Base para custom domain em GitHub Pages/Vercel/Netlify
+      base: '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
